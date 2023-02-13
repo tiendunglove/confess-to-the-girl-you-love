0797c28114f0f6efbfdbf2be6e136a65481c665d
tiendunglove <94985083+tiendunglove@users.noreply.github.com>
2023-02-13 13:57:51 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -9,8 +9,7 @@ const textConfig = {
   text8: "Gửi cho tớ <3",
   text9: "Vì cậu đẹp try vlllll",
   text10: "Tớ biết mà ^^ Yêu cậu 300.000",
-  text11:
-    "Inbox nhé, Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
+  text11:"Inbox nhé, Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
   text12: "Okii lunn <3",
 };
 
