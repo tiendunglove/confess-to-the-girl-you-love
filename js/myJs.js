54bf9abba03fdedba8ade49cb839f4e42cccf63e
tiendunglove <94985083+tiendunglove@users.noreply.github.com>
2023-06-20 23:51:52 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -137,7 +137,7 @@ $(document).ready(function () {
           text: textConfig.text11,
           confirmButtonColor: "#83d0c9",
           onClose: () => {
-            window.location = "https://mez.ink/meek30102003";
+            window.location = "https://linkbio.co/tiendung2k03";
           },
         });
       }
